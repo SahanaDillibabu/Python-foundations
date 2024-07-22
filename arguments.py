b4cad5af57c887b15a77b5349a5ab2a19878c32c
Sahana D. <sahanadillibabu.intern@gmail.com>
2024-07-22 17:31:48 -0500

    Add files via upload

---
+++ arguments.py
@@ -0,0 +1,7 @@
+def greet(first_name, last_name): #defining 2 parameters/arguments in brackets
+    print(f"Hello {first_name} {last_name}")
+    print("Welcome Aboard")
+
+
+greet("John", "Doe")
+greet("Lucy", "Willis")
