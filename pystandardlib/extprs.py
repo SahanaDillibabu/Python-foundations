b9b2be46a0f874aa83979d4537cb89c4e3529ae3
Sahana D. <sahanadillibabu.intern@gmail.com>
2024-08-06 16:21:09 -0500

    Add files via upload

---
+++ pystandardlib/extprs.py
@@ -0,0 +1,11 @@
+import subprocess
+
+completed = subprocess.run(["python3","other.py"],
+                            capture_output=True,
+                            text=True)
+print("args",completed.args)
+print("returncode",completed.returncode)
+print("stderr",completed.stderr)
+print("stdout",completed.stdout)
+if completed.returncode != 0:
+    print(completed.stderr)
