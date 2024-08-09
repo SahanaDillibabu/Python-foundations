79978567c225bc4f5d78edd59ceff0b07fabd6ab
Sahana D. <sahanadillibabu.intern@gmail.com>
2024-08-08 19:51:01 -0500

    Add files via upload

---
+++ pystandardlib/docstrings.py
@@ -0,0 +1,16 @@
+#practice for docstrings, not an actual converter
+
+"""This module provides functions to convert a pdf to text."""
+class Converter:
+    """A simple converter for converting pdfs to text."""
+    def convert(path):
+        """
+        Convert the given pdf to text.
+
+        Parameters:
+        path(str): the path to a PDF file.
+
+        returns:
+        str: The content of the PDF file as text.
+        """
+    print("pdf2text")
